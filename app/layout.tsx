1b37d69955f5f1ab17d32cd22c86978a160d9d06
EpicIbby101 <kevin_grittner@outlook.com>
2024-05-30 11:01:15 +0100

    Added sign-up and sign-in pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { getSEOTags } from "@/libs/seo";
 import ClientLayout from "@/components/LayoutClient";
 import config from "@/config";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const font = Inter({ subsets: ["latin"] });
 
@@ -22,16 +23,22 @@ export const metadata = getSEOTags();
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" data-theme={config.colors.theme} className={font.className}>
-      {config.domainName && (
-        <head>
-          <PlausibleProvider domain={config.domainName} />
-        </head>
-      )}
-      <body>
-        {/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
-        <ClientLayout>{children}</ClientLayout>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html
+        lang="en"
+        data-theme={config.colors.theme}
+        className={font.className}
+      >
+        {config.domainName && (
+          <head>
+            <PlausibleProvider domain={config.domainName} />
+          </head>
+        )}
+        <body>
+          {/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
+          <ClientLayout>{children}</ClientLayout>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
